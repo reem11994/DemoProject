2204522520ab600611439f1c90b9f9aa80f0b043
reem11994 <reem112jazi@gmail.com>
2016-10-17 23:26:47 +0300

    AuthAccessToken

--- DemoProject/src/main/java/demo/Test.java
+++ DemoProject/src/main/java/demo/Test.java
@@ -2,20 +2,11 @@ package demo;
 import java.net.*;
 import java.io.*;
 
-import twitter4j.Status;
-//import demo.ReadingTextFile;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+
 
 import java.util.List;
 
 
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.api.TweetsResources;
-import twitter4j.conf.Configuration;
-import twitter4j.conf.ConfigurationBuilder;
 public class Test {
 	public static void main(String[] args) {
 	List<ProfileData> profileData;
@@ -56,47 +47,11 @@ String inputLine;
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+	}
+}
    
 	
-/*    Twitter twitter = get_twitter_instance();
-    try {
-     
-        List<Status> statuses;
-        String user;
-       
-//            user = "Izdihar Shneneh";
-//            statuses = twitter.getUserTimeline(user);
-            Query o= new Query("#عاجل");
-           
-          QueryResult pp=  twitter.search(o);
-      
-     
-        for (Status status : pp.getTweets()) {
-            System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-        }
-    } catch (TwitterException te) {
-        te.printStackTrace();
-        System.out.println("Failed to get timeline: " + te.getMessage());
-        System.exit(-1);
-    }*/
-}
 
 
-public static Twitter get_twitter_instance()
-{
-      ConfigurationBuilder cb = new ConfigurationBuilder();
-                               
-                                 cb.setOAuthConsumerKey("sPxuPeP9I1YQf43WowJEiBOSI")
-                                 .setOAuthConsumerSecret("SQI20VhoCLRArpLeqotNhSG03Lk4v5roer5tVsxiBJcQ67U2Dz")
-                                 .setOAuthAccessToken("755476527890964480-Bg3A1FBfCZVQD7L3hemLw9lcvgJpVnm")
-                                 .setOAuthAccessTokenSecret("5rsn19DYp9iRP07oxlj63nvUn4B1nUhRcS9CKQFYNmzuu")
-                                        .setJSONStoreEnabled(true);
-                                
-                                 TwitterFactory tf = new TwitterFactory(cb.build());  
-                                
-                                Twitter twitter = tf.getInstance();
-                                return twitter;
-}
 
 
-}
---
+++ DemoProject/src/test/java/com/graduationProject/tests/TwitterFunctionsTest.java
@@ -0,0 +1,5 @@
+package com.graduationProject.tests;
+
+public class TwitterFunctionsTest {
+
+}
